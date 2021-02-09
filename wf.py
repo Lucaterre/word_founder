955f200405dc3d021cf3b1a6eb6ec5a9717b820b
Lucas Terriel <lucas.terriel@inria.fr>
2021-02-09 17:44:23 +0100

    add source

---
+++ wf.py
@@ -0,0 +1,50 @@
+
+def get_offset_length_word():
+	def rerun():
+		answer = input('Do you want continue ? [O/n]\n\n')
+		if answer.lower() == 'o':
+			run = True
+		else:
+			run = False
+		return run
+
+	print("""
+		|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+		|***********************************************|
+		|**************** WORD FOUNDER *****************|
+		|***********************************************|
+		|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+
+		HELP
+		----
+
+		Found the offset and the length of your your word
+		in sentence
+
+		\n""")
+	run = True
+
+	while run:
+		try :
+			sentence = input('1) Enter your complete sentence : \n\n')
+			print('-' * 20, '\n')
+			word_to_find = input('2) Enter the word want to find in sentence : \n\n')
+			print('-' * 20, '\n')
+			offset_start_sentence = int(input('3) Enter the offset start sentence : \n\n'))
+			print('-' * 20, '\n')
+			if offset_start_sentence == None:
+				offset_start_sentence = 0
+			offset_start_word = offset_start_sentence + sentence.index(word_to_find)
+			length = len(word_to_find)
+			print(f'[RESULT :] offset start word : {offset_start_word} | length of your word : {length} \n') 
+			run = rerun()
+
+		except ValueError as E:
+			print(f"Your word didn't match in sentence | {E} \n")
+			print("Try again ! \n")
+
+			run = rerun()
+
+
+
+get_offset_length_word()
